628f5e360057e3c529f1d41654ab478f230ec18c
Adriel Bernhard <adrielsmi79@gmail.com>
2025-08-08 09:29:07 +0700

    update B14

--- Bagian B/B14.php
+++ Bagian B/B14.php
@@ -8,15 +8,29 @@
   $b = 3;
   $c = 5;
   
+  if ($c < $a || $c < $b) {
+    if ($a > $b) {
+      $temp = $c;
+      $c = $a;
+      $a = $temp;
+    } else {
+      $temp = $c;
+      $c = $b;
+      $b = $temp;
+    }
+  }
+  
   $A = $a ** 2;
   $B = $b ** 2;
   $C = $c ** 2;
   
   if ($A + $B == $C){
-    echo "3 angka ini adalah Triplet Phytagoras benar";
+    $hasil = "benar";
   } else {
-    echo "3 angka ini adalah Triplet Phytagoras salah";
+    $hasil = "salah";
   }
+
+  echo "3 angka ini adalah Triplet Phytagoras $hasil"
   
 ?>
 
